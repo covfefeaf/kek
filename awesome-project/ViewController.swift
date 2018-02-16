4e3253f543735fed33a30d34e0d4aef277a14422
Anton Belyaev <partyordie@icloud.com>
2018-02-16 23:07:06 +0500

    Implement basic plane detecting.

--- awesome-project/ViewController.swift
+++ awesome-project/ViewController.swift
@@ -2,31 +2,65 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
-
-    @IBOutlet var sceneView: ARSCNView!
+class ViewController: UIViewController {
+    
+    var source: SCNAudioSource? = nil
+    
+    @IBOutlet private weak var arView: ARSCNView! {
+        didSet {
+            let rec = UITapGestureRecognizer(target: self, action: #selector(didTap))
+            arView.addGestureRecognizer(rec)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
-        sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        let scene = SCNScene(named: "art.scnassets/main.scn")!
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
-        sceneView.session.run(configuration)
+        let conf = ARWorldTrackingConfiguration()
+        arView.session.run(conf, options: [])
+        
+        arView.autoenablesDefaultLighting = true
+        arView.automaticallyUpdatesLighting = true
+        
+        conf.planeDetection = .horizontal
+        arView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        
+        arView.delegate = self
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        arView.session.pause()
+    }
+    
+    @objc func didTap(_ rec: UITapGestureRecognizer) {
+        let pt = rec.location(in: arView)
+        let res = arView.hitTest(pt, options: [.boundingBoxOnly: true]).flatMap { $0.node }
+        for node in res {
+            node.removeFromParentNode()
+        }
+        if !res.isEmpty, let src = source {
+            arView.scene.rootNode.addAudioPlayer(SCNAudioPlayer(source: src))
+        }
+    }
 }
+
+extension ViewController: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {
+            return
+        }
+        let sphere = SCNSphere(radius: 0.05)
+        let sphereNode = SCNNode(geometry: sphere)
+        let sphereNode2 = SCNNode(geometry: sphere)
+        sphereNode.position = SCNVector3(planeAnchor.center.x, planeAnchor.center.y, planeAnchor.center.z)
+        sphereNode2.position = SCNVector3((planeAnchor.center.x + 0.1), planeAnchor.center.y, planeAnchor.center.z)
+        node.addChildNode(sphereNode)
+        node.addChildNode(sphereNode2)
+    }
+}
+
